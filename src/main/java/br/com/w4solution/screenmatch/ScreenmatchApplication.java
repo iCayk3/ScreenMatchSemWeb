a381f946b53ba89166390619bb2d51f779eb1096
Cayke Silva <silvacayke@gmail.com>
2024-06-25 20:21:17 -0300

    Alteracoes na API

--- src/main/java/br/com/w4solution/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/w4solution/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.w4solution.screenmatch;
 
+import br.com.w4solution.screenmatch.model.DadosSerie;
 import br.com.w4solution.screenmatch.service.ConsumoAPI;
+import br.com.w4solution.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,9 +17,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&Season=1&apikey=46a057c");
+		var json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&apikey=46a057c");
 //		System.out.println(json);
 //		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(json);
+		var conversorDados = new ConverteDados();
+		var dadosSerie = conversorDados.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
 	}
 }
